a1fc5c3c1e794c674316860b99288b9e2d745fc5
Smiti <mishra.smiti06@gmail.com>
2021-09-23 17:09:20 -0400

    tested with mocha, refactor the code and learened to fetch API

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,35 @@
 const request = require('request');
-const breedName = process.argv[2];
 
-let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-request(url, (error, response, body) => {
+
+const fetchBreedDescription = function(breedName, callback) {
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
     
-  if (error) {
-    console.log(error);
-    return error;
+    if (error) {
+      console.log(error);
+      return error;
+    }
+  
+    const data = JSON.parse(body);
+    console.log(response);
+
+    if (data.length === 0) {
+      errMsg = `Breed ${breedName} not found`;
+      callback(errMsg, null);
+    } else {
+      callback(null, data[0].description);
+    }
+     
+  });
+};
+const callback = function (errMsg, body){
+  if(errMsg){
+    console.log(errMsg);
+  } else{
+    console.log(body);
   }
 
-  const data = JSON.parse(body);
-  console.log(data);
-  if (data.length === 0) {
-    console.log("breed not found");
-  }
-   
-});
+}
+fetchBreedDescription('kalu', callback);
+
+module.exports = fetchBreedDescription ;
